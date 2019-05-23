6203f0cf0203fb7d16ff10bb0d7b8a430ade8448
Christopher Hewett <chewett@hotmail.co.uk>
2019-05-23 23:14:58 +0100

    Big update to make this much more easy to use and friendlier

--- src/DiscordWebhookPoster.php
+++ src/DiscordWebhookPoster.php
@@ -2,16 +2,31 @@
 
 namespace Chewett\PHPDiscordWebhookMessenger;
 
+use Chewett\PHPDiscordWebhookMessenger\Exceptions\DiscordCurlException;
 
+/**
+ * Class DiscordWebhookPoster
+ *
+ * Simple class with a single static method used to send a DiscordWebhookPayload.
+ *
+ * @package Chewett\PHPDiscordWebhookMessenger
+ */
 class DiscordWebhookPoster
 {
 
 
     /**
+     * Static method used to send a DiscordWebhookPayload via Curl.
+     *
+     * Note: The most common failure for this is curl not having access to SSL certificates
+     * TODO: Put a piece in the notes about this.
+     *
      * @param string $webhookUrl URL of the channel webhook to post JSON to
      * @param DiscordWebhookPayload $payloadObject Object representing the
+     * @return string Web response from Discord when successful, otherwise an exception is thrown.
+     * @throws DiscordCurlException Thrown when curl has an issue and fails to make the request.
      */
-    public static function postMessage($webhookUrl, $payloadObject) {
+    public static function postMessage($webhookUrl, DiscordWebhookPayload $payloadObject) {
         $jsonToUpload = $payloadObject->getPayload();
 
         $ch = \curl_init($webhookUrl);
@@ -23,7 +38,13 @@ class DiscordWebhookPoster
             'Content-Length: ' . strlen($jsonToUpload)
         ]);
 
-        return \curl_exec($ch);
+        $result = \curl_exec($ch);
+        if($result) {
+            return $result;
+        }
+
+        $curlError = \curl_error($ch);
+        throw new DiscordCurlException($ch, $curlError);
     }
 
 
